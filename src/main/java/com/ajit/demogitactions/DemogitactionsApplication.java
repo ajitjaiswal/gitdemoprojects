5e20be73be271c183ece612b8a4834e9c0787d43
Ajit <ajitjaiswal@hotmail.com>
2023-08-26 16:29:27 +0530

    First commit

---
+++ src/main/java/com/ajit/demogitactions/DemogitactionsApplication.java
@@ -0,0 +1,13 @@
+package com.ajit.demogitactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemogitactionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemogitactionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ajit/demogitactions/DemogitactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajit.demogitactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemogitactionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
